25b4495190b2a7769c72ed6d0db8dbb08815e249
Narayana <narayana.vadapalli1996@gmail.com>
2023-03-08 17:15:32 +0530

    Update production.py

--- apiserver/plane/settings/production.py
+++ apiserver/plane/settings/production.py
@@ -43,17 +43,10 @@ DOCKERIZED = os.environ.get(
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
-# Allow all host headers
-ALLOWED_HOSTS = ["http://localhost:8080","http://localhost:80","http://localhost"]
 
 # TODO: Make it FALSE and LIST DOMAINS IN FULL PROD.
 CORS_ALLOW_ALL_ORIGINS = True
 
-CORS_ALLOWED_ORIGINS = ["http://localhost:8080","http://localhost:80","http://localhost"]
-
-CORS_ALLOWED_ORIGIN_REGEXES = [
-r"^https://\w+\.domain\.com$",
-]
 
 CORS_ALLOW_METHODS = [
 'DELETE',
