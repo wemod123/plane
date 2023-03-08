4274951c73ca77972005c4ae2e3432a2c114c917
Narayana <narayana.vadapalli1996@gmail.com>
2023-03-08 16:53:41 +0530

    .

--- apiserver/plane/settings/common.py
+++ apiserver/plane/settings/common.py
@@ -21,6 +21,7 @@ INSTALLED_APPS = [
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
+    "corsheaders",
     # Inhouse apps
     "plane.analytics",
     "plane.api",
